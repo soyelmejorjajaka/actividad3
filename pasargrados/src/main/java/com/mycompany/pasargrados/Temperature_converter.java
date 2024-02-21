40ccc5a6559426683088aeebd7259a7919830ae0
soyelmejorjajaka <adrian.teransanchez@iesmiguelherrero.com>
2024-02-21 15:33:36 +0100

    SEXO

--- pasargrados/src/main/java/com/mycompany/pasargrados/Temperature_converter.java
+++ pasargrados/src/main/java/com/mycompany/pasargrados/Temperature_converter.java
@@ -6,18 +6,6 @@ public class Temperature_converter {
 	System.out.print("Enter value which you want to convert : ");
 	float value = scan.nextFloat();
 	System.out.println("  Enter 1 for Celsius to Fahrenheit converter....");
-	System.out.println("  Enter 2 for Fahrenheit to Celsius converter....");
+	System.out.println("  Enter 2 for WPSOWEFKO to Celsius converter....");
 	System.out.print("\nEnter your value here...   ");
 	int ans = scan.nextInt();
-	if (ans == 1) {
-		float value1 = (float) ((value*(1.8))+32);
-		System.out.print("Your conversion is here.. "+value1+"°F");
-		}
-	if (ans == 2) {
-			float value2 = (float) (value-32);
-			float value3 = (float) (value2*(5)/9);
-			System.out.print("Your conversion is here.. "+value3+"°C");
-		}
-	System.out.println("\n\tThank You!!..");
-	}
-}
